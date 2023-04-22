2e5eff26d53398b400461d1c8070dd33eb17b50c
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-04-22 13:44:30 +0300

    first step for adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,17 @@ export function Header({ headerClass }) {
             behavior: 'smooth'
         });
     };
+
+    const showNav = () => {
+        const burgerNav = document.querySelector(`.${style.burgerNav}`);
+        if (burgerNav.style.visibility === 'visible') {
+            burgerNav.style.visibility = 'hidden';
+            burgerNav.style.transform = 'translateX(100vw)'
+        } else {
+            burgerNav.style.visibility = 'visible';
+            burgerNav.style.transform = 'translateX(0vw)';
+        }
+    };
     return (
         <header className={`${style.header} ${headerClass}`}>
             <div className={style.headerWrapper}>
@@ -40,6 +51,38 @@ export function Header({ headerClass }) {
                     <a href="#">Resume</a>
                 </div>
             </nav>
+            <div className={style.burgerMenuWrapper}>
+                <button onClick={showNav}  className={style.burgerMenuButton}>
+                    <div className={style.burgerMenu}></div>
+                </button>
+                <aside className={style.burgerNav}>
+                    <nav>
+                        <ol start='0'>
+                            <li >
+                                <a className={style.headerLink} href="#about" onClick={(event) => {
+                                    event.preventDefault();
+                                    scrollToComponent('about');
+                                }}>Обо мне</a>
+                            </li>
+                            <li>
+                                <a className={style.headerLink} href="#projects" onClick={(event) => {
+                                    event.preventDefault();
+                                    scrollToComponent('projects');
+                                }}>Мои проекты</a>
+                            </li>
+                            <li>
+                                <a className={style.headerLink} href="#contact" onClick={(event) => {
+                                    event.preventDefault();
+                                    scrollToComponent('contact');
+                                }}>Связаться со мной</a>
+                            </li>
+                        </ol>
+                        <div className={style.headerResume}>
+                            <a href="#">Resume</a>
+                        </div>
+                    </nav>
+                </aside>
+            </div>
         </header>
     )
 }

b3039dec0065027d6f9dd3966e75308fb3aeb90d
Vitaly Kryanin <92264488+VKryanin@users.noreply.github.com>
2023-04-27 18:39:05 +0300

    fix burgermenu

--- src/components/Header/BurgerMenu/BurgerMenu.jsx
+++ src/components/Header/BurgerMenu/BurgerMenu.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 import style from './BurgerMenu.module.css'
 
 export function BurgerMenu() {
@@ -11,36 +10,48 @@ export function BurgerMenu() {
         });
     };
     const showNav = () => {
+        const body = document.querySelector('body');
+        const burgerMenu = document.querySelector(`.${style.burgerMenu}`);
+        const burgerMenuClose = document.querySelector(`.${style.burgerMenuClose}`)
         const burgerNav = document.querySelector(`.${style.burgerNav}`);
+
         if (burgerNav.style.visibility === 'visible') {
+            body.style.overflow = 'auto';
             burgerNav.style.visibility = 'hidden';
-            burgerNav.style.transform = 'translateX(100vw)'
+            burgerNav.style.transform = 'translateX(100vw)';
+            burgerMenuClose.style.visibility = 'hidden';
+            burgerMenu.style.visibility = 'visible';
+
         } else {
+            body.style.overflow = 'hidden';
             burgerNav.style.visibility = 'visible';
             burgerNav.style.transform = 'translateX(0vw)';
+            burgerMenuClose.style.visibility = 'visible';
+            burgerMenu.style.visibility ='hidden'
         }
     };
     return (
         <div className={style.burgerMenuWrapper}>
             <button onClick={showNav} className={style.burgerMenuButton}>
                 <div className={style.burgerMenu}></div>
+                <div className={style.burgerMenuClose}></div>
             </button>
             <aside className={style.burgerNav}>
                 <nav>
                     <ol start='0'>
-                        <li >
+                        <li onClick={showNav}>
                             <a href='/about' className={style.headerLink} onClick={(event) => {
                                 event.preventDefault();
                                 scrollToComponent('about');
                             }}>Обо мне</a>
                         </li>
-                        <li>
+                        <li onClick={showNav}>
                             <a className={style.headerLink} href="#projects" onClick={(event) => {
                                 event.preventDefault();
                                 scrollToComponent('projects');
                             }}>Мои проекты</a>
                         </li>
-                        <li>
+                        <li onClick={showNav}>
                             <a className={style.headerLink} href="#contact" onClick={(event) => {
                                 event.preventDefault();
                                 scrollToComponent('contact');
